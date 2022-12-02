63af91f287af24d88328786059b489e931f86b6b
Jean-Marc <null>
2022-12-02 09:18:04 +0100

    add header

---
+++ GFXFonts.h
@@ -0,0 +1,10 @@
+/**
+ * this file is a place holder for compatibility with arduino library
+ * all font files will be call  in Fonts directory
+ * */
+
+#ifndef __GFXFONTS
+
+#define __GFXFONTS
+
+#endif
